1c647b6a62a3c6d80184e4a909e5307811819916
Joshua Thomas <personettelabs@gmail.com>
2022-08-21 22:05:55 -0400

    removes computer

--- src/project/scenes/root_scenes/level5.gd
+++ src/project/scenes/root_scenes/level5.gd
@@ -4,7 +4,7 @@ extends "res://scenes/root_scenes/WorldTemplate.gd"
 func _init():
 	map = [
 		[w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,D,w,w],
-		[w,o,o,w,w,w,w,w,w,o,r,P,C,r,w,o,o,o,C,w],
+		[w,o,o,w,w,w,w,w,w,o,r,P,w,r,w,o,o,o,C,w],
 		[w,o,w,w,g,o,o,w,w,w,R,w,w,w,w,o,o,o,o,w],
 		[w,o,r,o,o,w,o,R,R,R,r,w,w,w,w,w,w,R,w,w],
 		[w,w,r,w,r,o,o,w,w,w,r,w,w,w,w,w,w,o,w,w],
