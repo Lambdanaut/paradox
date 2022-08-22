51994f6852db2c34205244fdac52da454699c637
Joshua Thomas <personettelabs@gmail.com>
2022-08-21 22:09:20 -0400

    level8 change

--- src/project/scenes/root_scenes/Level8.gd
+++ src/project/scenes/root_scenes/Level8.gd
@@ -5,13 +5,20 @@ func _init():
 	map = [
 		[w,w,w,w,w,w,w,w,w,P,w,w,w,w,w,w,w,w,w,w],
 		[w,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,w],
+		[w,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,w],
+		[w,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,w],
+		[w,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,w],
 		[w,o,o,o,o,o,o,o,o,C,o,o,o,o,o,o,o,o,o,w],
 		[w,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,w],
 		[w,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,w],
 		[w,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,w],
 		[w,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,w],
-		[w,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,w],
-		[w,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,w],
-		[w,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,w],
 		[w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w],
+	]
+
+	terminal_dialogue = [
+		"Incoming message from Lambdanaut",
+		"[color=#ffe3c8]YOU BEAT THE DEMO WHAT?! WITHOUT ANY SAVE FILES OR ANYTHING?[/color]",
+		"[color=#ffe3c8]You're an actual hero! Please do message and let me know that you got this far, and congratulations![/color]",
+		"[color=#ffe3c8]So much love[/color]\n-Lambda",
 	]
