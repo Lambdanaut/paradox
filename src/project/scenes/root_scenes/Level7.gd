a42d6f2d2db6f0dbddf85218eb6133e12b44f159
Joshua Thomas <personettelabs@gmail.com>
2022-08-22 20:28:47 -0400

    beeg button

--- src/project/scenes/root_scenes/Level7.gd
+++ src/project/scenes/root_scenes/Level7.gd
@@ -4,12 +4,12 @@ extends "res://scenes/root_scenes/WorldTemplate.gd"
 func _init():
 	map = [
 		[w,w,D,w,w,w,w,w,w,P,w,w,w,w,w,w,w,w,w,w],
-		[w,w,o,w,w,w,w,w,o,o,o,w,w,w,w,w,w,w,w,w],
-		[w,w,R,w,R,G,R,G,o,o,o,G,G,R,R,G,G,o,o,w],
-		[w,w,G,w,G,w,w,w,o,o,r,w,w,w,w,w,o,B,o,w],
-		[w,w,R,w,R,w,w,w,o,B,g,r,w,w,w,w,o,r,o,w],
-		[w,w,G,w,G,w,w,w,o,o,o,o,w,w,w,w,o,o,r,w],
-		[w,w,R,w,R,w,w,w,w,w,w,w,w,w,C,w,w,w,g,w],
+		[w,w,o,w,w,w,w,w,o,o,o,w,w,w,w,o,o,w,w,w],
+		[w,w,R,w,R,G,R,G,o,o,o,G,G,R,R,o,o,w,o,w],
+		[w,w,G,w,G,w,w,w,o,o,r,w,w,w,w,B,o,o,o,w],
+		[w,w,R,w,R,w,w,w,g,B,g,r,G,G,G,R,o,o,o,w],
+		[w,w,G,w,G,w,w,w,o,o,o,o,w,w,w,w,w,w,w,w],
+		[w,w,R,w,R,w,w,w,w,w,w,w,w,w,C,w,w,w,w,w],
 		[w,w,G,R,G,w,w,w,w,w,w,w,w,w,o,w,w,w,w,w],
 		[w,w,o,o,o,o,o,o,o,o,o,o,o,o,o,w,w,w,w,w],
 		[w,w,o,o,o,o,o,o,o,o,o,o,o,o,o,w,w,w,w,w],
