bd09b293690c82ffce676ccbc9f75d8a7093acd0
Joshua Thomas <personettelabs@gmail.com>
2022-09-08 12:04:34 -0400

    sprite edit

--- src/project/scenes/root_scenes/Level8.gd
+++ src/project/scenes/root_scenes/Level8.gd
@@ -4,7 +4,7 @@ extends "res://scenes/root_scenes/WorldTemplate.gd"
 func _init():
 	map = [
 		[w,w,w,w,P,w,w,w,w,w,w,w,w,w,w,w,w,D,w,w],
-		[w,w,o,o,C,o,w,w,w,w,w,w,w,w,w,w,o,o,o,w],
+		[w,w,o,o,o,o,w,w,w,w,w,w,w,w,w,w,o,o,o,w],
 		[w,w,o,o,o,o,w,w,w,w,w,w,w,w,w,w,o,o,o,w],
 		[w,w,o,o,r,o,w,w,o,G,G,o,r,w,w,w,o,o,o,w],
 		[w,w,o,o,B,o,R,R,o,G,G,o,r,w,w,w,o,o,o,w],
